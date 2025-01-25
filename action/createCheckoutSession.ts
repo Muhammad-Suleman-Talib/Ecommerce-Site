ab04638b62ff84ef8566ce6964aa21808d230a07
Muhammad-Suleman-Talib <suleman999ah@gmail.com>
2025-01-25 22:59:48 +0500

    add the files

--- action/createCheckoutSession.ts
+++ action/createCheckoutSession.ts
@@ -50,6 +50,7 @@ function extractPlainText(description: any): string {
       allow_promotion_codes: true,
       success_url: `https://iphone-store-online.vercel.app/success?session_Id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`,
       cancel_url: `https://iphone-store-online.vercel.app/basket`,
+      
       line_items: items.map((item) => ({
         price_data: {
           currency: "usd",
